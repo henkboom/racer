e164030a2aaa2ef1df373e956116c6cd5199fa67
Henk Boom <henk@henk.ca>
2010-09-07 11:26:43 -0400

    larger player ship

--- components/blueprints.lua
+++ components/blueprints.lua
@@ -2,9 +2,9 @@ local collision = require 'dokidoki.collision'
 local v2 = require 'dokidoki.v2'
 
 player_ship = game.make_blueprint('player_ship',
-  {'transform', scale_x=1/4, scale_y=1/4},
+  {'transform', scale_x=1/2, scale_y=1/2},
   {'sprite', resource='ship_sprite'},
-  {'collider', class='ship', poly=collision.make_rectangle(3, 2)},
+  {'collider', class='ship', poly=collision.make_rectangle(6, 4)},
   {'player_ship_control'},
   {'ship'})
 
